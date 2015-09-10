6de31ad6ab967a330b0e8357c76fdaff298b9d32
Jimmy Xu <jianmin.xu@usingnow.com>
2015-09-10 19:11:06 +0800

    fixes the routes issue of search_page_of.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :free_insurances do
-    member do
-      get :search_page_of
-    end
+    # member do
+    #   get :search_page_of
+    # end
     collection do
       get :search_result_of
       get :validate_result_of
@@ -22,6 +22,7 @@ Rails.application.routes.draw do
 
   get 'validate_user_of' => 'free_insurances#validate_user_of', as: :validate_user_of
   get 'all_terms' => 'free_insurances#all_terms', as: :all_terms
+  get 'search_page_of' => 'free_insurances#search_page_of', as: :search_page_of
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
